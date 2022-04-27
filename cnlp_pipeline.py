2ba6317355f4e1f9d6652055eb87a7f04f384550
Eli Goldner <egoldner@brandeis.edu>
2022-04-27 11:28:14 -0400

    modified:   cnlp_pipeline.py

--- cnlp_pipeline.py
+++ cnlp_pipeline.py
@@ -12,12 +12,40 @@ class PipelineArguments:
     Arguments pertaining to the models, mode, and data used by the pipeline.
     """
     model_dir: str = field(
-        "Path where each entity model is stored in a folder named after its tag suffix, e.g. 'med' for 'B-med I-med', and the relation extraction model stored in a folder named 'relex'"
+        metadata={
+            "help" : (
+                "Path where each entity model is stored in a folder named after its tag suffix,"
+                "e.g. 'med' for 'B-med I-med', and the relation extraction model stored in a folder named 'relex'"
+            )
+        }
     )
     in_file: str = field(
-        "Path to file with one raw sentence per line in the case of inference and one <label>\t<annotated sentence> per line in the case of evaluation"
+        metadata = {
+            "help" : (
+                "Path to file with one raw sentence per line in the case of inference,"
+                " and one <label>\t<annotated sentence> per line in the case of evaluation"
+            )
+        }
     )
     mode: str = field(
-        "Use mode for full pipeline, inference which outputs annotated sentences and their relation, or eval which outputs metrics for a provided set of samples (requires labels)"
+        default = "inf",
+        metadata = {
+            "help" : (
+                "Use mode for full pipeline, inference which outputs annotated sentences and their relation,"
+                "or eval which outputs metrics for a provided set of samples (requires labels)"
+            )
+        }
+    )
+    tokenizer: str = field(
+        default="roberta-base",
+        metadata = {
+            "help" : (
+                "At the moment, assuming everything has been trained using the same tokenizer."
+                "Figuring out ways around this later might be annoying, including saving each tokenizer"
+                "in each model dir"
+            )
+        }
     )
     
+def main():
+    parser = HfArgumentParser((PipelineArguments))
