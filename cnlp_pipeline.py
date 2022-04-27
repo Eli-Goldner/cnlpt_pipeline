eb3aff037a84455a53054baff57438b2010e8353
Eli Goldner <egoldner@brandeis.edu>
2022-04-27 11:16:40 -0400

    new file:   cnlp_pipeline.py

---
+++ cnlp_pipeline.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass, field
+
+from .cnlp_pipeline_utils import TaggingPipeline, get_sentences_and_labels
+
+from transformers import HfArgumentParser
+
+modes=["inf", "eval"]
+
+@dataclass
+class PipelineArguments:
+    """
+    Arguments pertaining to the models, mode, and data used by the pipeline.
+    """
+    model_dir: str = field(
+        "Path where each entity model is stored in a folder named after its tag suffix, e.g. 'med' for 'B-med I-med', and the relation extraction model stored in a folder named 'relex'"
+    )
+    in_file: str = field(
+        "Path to file with one raw sentence per line in the case of inference and one <label>\t<annotated sentence> per line in the case of evaluation"
+    )
+    mode: str = field(
+        "Use mode for full pipeline, inference which outputs annotated sentences and their relation, or eval which outputs metrics for a provided set of samples (requires labels)"
+    )
+    
